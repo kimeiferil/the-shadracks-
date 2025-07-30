c06c1119b1abc0a5813ba275c71fc1b226a8e48a
kimeiferil <fetrogames1@gmail.com>
2025-07-30 04:37:14 -0400

    Create Album.js

---
+++ server/models/Album.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const albumSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    coverPhoto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Photo'
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Album', albumSchema);
